a0955b290c322c3218d7aa78748328b5f5f57ac7
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-12-19 12:04:05 +0200

    added UI for login page

--- Restaurant-App-Expo/components/LoginPage.jsx
+++ Restaurant-App-Expo/components/LoginPage.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import { StyleSheet, TextInput, Text, Button, View, Alert } from 'react-native';
+import { StyleSheet, TextInput, Text, Button, View, Alert,Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginPage(){
+    const navigation = useNavigation();
 
     return(
         <div>
             <View style={styles.container}>
-            {/* <Image
-         source={require('@/assets/images/food-img.jpg')}
-         style={{width:180, height:180, justifyContent: 'center', marginBottom:35}}
-       /> */}
-       <img src="../assets/images/food-img.jpg" alt="" srcset="" width="350" />
+       <Image
+        source={require('@/assets/images/food-plate.jpg')} // Adjust the path to match your project structure
+        style={{ width: 350, height: 300, marginBottom: 35 , borderRadius:400/ 2}}
+      />
             <h1>Login</h1>
             <TextInput
-      //style={styles.input}
-      placeholder="Email"
+      style={styles.input}
+      placeholder="Username"
       //value={data.email}
       onChangeText={(value) => handleInput('email', value)}
       keyboardType="email-address"
     />
     <TextInput
-      //style={styles.input}
+      style={styles.input}
       placeholder="Password"
       //value={data.password}
       onChangeText={(value) => handleInput('password', value)}
       secureTextEntry
     />
     {/* <Button title="Sign Up" onPress={handleSubmit} /> */}
+    <p>Forgot password</p>
+    <p>New user: Signup</p>
             </View>
         </div>
     )
@@ -35,7 +38,18 @@ export default function LoginPage(){
 const styles = StyleSheet.create({
     container: {
       padding: 16,
-      backgroundColor:'#FF4C52'
+      backgroundColor:'#FF4C52',
+      flex: 1, 
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    input:{
+        borderColor:'#ffffff',
+        height: 40,
+        width: "50%",
+        padding:25,
+        margin:2,
+        borderRadius:25
     }
     
   });
