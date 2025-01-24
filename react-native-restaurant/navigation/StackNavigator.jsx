7059520f0a57453945ca3420a8151dd7a464c85a
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-01-24 11:48:47 +0200

    fixed error

--- react-native-restaurant/navigation/StackNavigator.jsx
+++ react-native-restaurant/navigation/StackNavigator.jsx
@@ -1,8 +1,8 @@
 import HomePage from "@/components/HomePage";
 import LoginPage from "@/components/LoginPage";
 import RegistrationPage from "@/components/RegistrationPage";
-import { createStackNavigator } from "@react-navigation/stack";w
-import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,6 @@ export default function StackNavigator() {
   console.log("stack");
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="../app/(tabs)/_layout.tsx" component={HomePage} /> */}
         <Stack.Screen name="HomePage" component={HomePage} />
       <Stack.Screen name="RegistrationPage" component={RegistrationPage} />
       <Stack.Screen name="LoginPage" component={LoginPage} />
